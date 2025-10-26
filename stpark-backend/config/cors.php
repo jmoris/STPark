d92e1c531ed8055e9c5c8ac0f10229ea1ce3fc44
Jesus Moris <jesus@soluciontotal.cl>
2025-10-26 14:43:55 -0300

    fix cors

--- stpark-backend/config/cors.php
+++ stpark-backend/config/cors.php
@@ -10,7 +10,7 @@ return [
     ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
-    'exposed_headers' => [],
-    'max_age' => 0,
+    'exposed_headers' => ['Authorization', 'X-Tenant'],
+    'max_age' => 86400,
     'supports_credentials' => true,
 ];
